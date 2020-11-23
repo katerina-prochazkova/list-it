6c96e6d9dfb8d57aa8815d87da414cbd92ccf71d
katerina-prochazkova <kat.prochazkova@gmail.com>
2020-11-23 20:24:53 +0100

    první commit

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.html';
+import './style.css';
+import 'db.js';
+
+render(
+  <>
+    <header>
+      <h1>Sdílený seznam</h1>
+    </header>
+    <main>
+      <p>tady bude sdílený seznam Tinky a Kačky</p>
+    </main>
+    <footer>by KP+KP</footer>
+  </>,
+  document.querySelector('#app'),
+);
