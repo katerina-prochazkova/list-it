a2551e5e3d36e3bd52c0495d33e309a358fc92fe
Kristina Petrickova <kristina.petrickova@gmail.com>
2020-11-28 13:47:00 +0100

    Komponenta NewFormItem

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { ListCategory } from '../ListCategory/index.jsx';
+import { NewItemForm } from '../NewItemForm/index.jsx';
 
 export const List = (props) => {
-  const [itemName, setItemName] = useState('');
-  const [itemAmount, setItemAmount] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setItemName('');
-    setItemAmount('');
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   setItemName('');
+  //   setItemAmount('');
+  // };
 
   return (
-    <body>
+    <>
       <header>
         <div className="navigation-container">
           <button id="nav-btn" className="nav-btn"></button>
@@ -34,43 +32,11 @@ export const List = (props) => {
       <main className="list-main">
         <div className="seznam-container">
           <h2>Název seznamu</h2>
-          {/* název seznamu bude props input value z homepage */}
-          <form className="form-list" onSubmit={handleSubmit}>
-            <div className="new-input">
-              <input
-                className="input"
-                type="text"
-                placeholder="  Nová položka"
-                value={itemName}
-                onChange={(event) => setItemName(event.target.value)}
-              />
-              <br />
-              <input
-                className="input"
-                type="text"
-                placeholder="  Množství (volitelné)"
-                value={itemAmount}
-                onChange={(event) => setItemAmount(event.target.value)}
-              />
-              <br />
-            </div>
-            <div className="kategorie-ikonky">
-              kategorie ikonky
-              <img className="ikonky-volba" src="ikonka" />
-              <img className="ikonky-volba" src="ikonka" />
-              <img className="ikonky-volba" src="ikonka" />
-              <img className="ikonky-volba" src="ikonka" />
-              {/* tady by mohly být ikonky pro kategorie (nebo dropdown select) */}
-            </div>
-            <button className="btn-add-item" type="submit">
-              přidat
-            </button>
-            {/* <!-- tady bude ikonka zelené kolečko s pluskem--> */}
-          </form>
+
+          <NewItemForm />
           <ListCategory />
         </div>
       </main>
-      <footer>KPKP 2020</footer>
-    </body>
+    </>
   );
 };
