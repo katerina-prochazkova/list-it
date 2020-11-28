97be63d4abe1170b86c4d9451953f1d31571f52e
Kristina Petrickova <kristina.petrickova@gmail.com>
2020-11-28 12:29:28 +0100

    Rozbalovací tlačítko pro kategorii

--- src/pages/ListCategory/index.jsx
+++ src/pages/ListCategory/index.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { ListItem } from '..pages/List Item/index.jsx';
+import { ListItem } from '../ListItem/index.jsx';
 
 export const ListCategory = (props) => {
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <div className="kategorie-container">
       <div className="kategorie-list">
         <input className="input-checkbox-ktg" type="checkbox" />
-        <button className="btn-kategorie">
+        <button className="btn-kategorie" onClick={handleClick}>
           <img src="ikonka" /> tohle bude kategorie s rozbalovací ikonkou
           <img src="ikonka" />
         </button>
         <img className="ikonka-delete" src="ikonka-delete" />
         {/* <!-- na klik rozbalí a zabalí ten ul na klik se taky změní ta rozbal ikonka z trojuhelníku dolů na trojuhelník nahoru --> */}
       </div>
-      <ul className="list">
-        <ListItem name="Rohlíky" amount="5" />
-        <ListItem name="Jablka" amount="1kg" />
-        {/* list item se zmapuje */}
-      </ul>
+      {active ? (
+        <ul className="list">
+          <ListItem name="Rohlíky" amount="5" />
+          <ListItem name="Jablka" amount="1kg" />
+          {/* list item se zmapuje */}
+        </ul>
+      ) : null}
     </div>
   );
 };
